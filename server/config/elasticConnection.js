415b9f2dd2f1e4cfd137a8e8b06643544295d7f2
Richard Devitt <richyrychporter@hotmail.com>
2025-12-07 22:05:59 -0500

    Built search and filter functionality for foods with Elastic Search

--- server/config/elasticConnection.js
+++ server/config/elasticConnection.js
@@ -1,3 +1,10 @@
 //Establish connection to elasticSearch
 //Import elasticSearch Client
 // Export client
+import { Client } from '@elastic/elasticsearch';
+const client = new Client({
+  node: 'http://localhost:9200'
+  
+});
+
+export { client };
