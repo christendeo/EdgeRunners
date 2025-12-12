f2520bf7f28113eef1252bb4268df12250eedde8
Kayla Hartland <kaylanhartland@gmail.com>
2025-12-12 12:10:41 -0500

    Blog post graphql queries, files for blog components

---
+++ client/queries/blogQueries.js
@@ -0,0 +1,103 @@
+import {gql} from '@apollo/client';
+//graphql queries for the Blogs data collection
+
+const GET_BLOGS = gql`
+    query {
+        getAllBlogs {
+            _id
+            user_id
+            title
+            content
+            post_type
+            current_weight
+            created_at
+            updated_at
+        }
+    }
+`;
+
+const GET_BLOG = gql`
+    query getBlog($_id: String!) {
+        getBlogById (_id: $_id) {
+            _id
+            user_id
+            title
+            content
+            post_type
+            current_weight
+            created_at
+            updated_at
+        }
+    }
+`;
+
+const GET_USER_BLOGS = gql`
+    query getUserBlogs($user_id: String!) {
+        getBlogsByUser(user_id: $user_id) {
+            _id
+            user_id
+            title
+            content
+            post_type
+            current_weight
+            created_at
+            updated_at
+        }
+    }
+`;
+
+const CREATE_BLOG = gql`
+    mutation createBlog($user_id: String!, $title: String!, $content: String!, $post_type: String!) {
+        addBlog(user_id: $user_id, title: $title, content: $content, post_type: $post_type){
+            _id
+            user_id
+            title
+            content
+            post_type
+            current_weight
+            created_at
+            updated_at
+        }
+    }
+`;
+
+const UPDATE_BLOG = gql`
+    mutation updateBlog($_id: String!, $user_id: String!, $title: String, $content: String, $post_type: String) {
+        editBlog(_id: $_id, user_id: $user_id, title: $title, content: $content, post_type: $post_type) {
+            _id
+            user_id
+            title
+            content
+            post_type
+            current_weight
+            created_at
+            updated_at
+        }
+    }
+`;
+
+const DELETE_BLOG = gql`
+    mutation deleteBlog($_id: String!, $user_id: String!) {
+        removeBlog(_id: $_id, user_id: $user_id) {
+            _id
+            user_id
+            title
+            content
+            post_type
+            current_weight
+            created_at
+            updated_at
+        }
+    }
+`;
+
+let exported = {
+    GET_BLOGS,
+    GET_BLOG,
+    GET_USER_BLOGS,
+    CREATE_BLOG,
+    UPDATE_BLOG,
+    DELETE_BLOG
+};
+
+export default exported;
