67e9bbbbe64c48b7e41d390a8a853096f3fc98ad
Richard Devitt <richyrychporter@hotmail.com>
2025-12-11 21:42:22 -0500

    Added placeholder components for foods and built docker implementation for client

--- client/pages/foods/index.js
+++ client/pages/foods/index.js
@@ -1,7 +1,10 @@
+import FoodSearch from '../../src/components/FoodSearch';
+
 export default function FoodsPage() {
     return (
         <div>
             <h1>Food Search</h1>
+            <FoodSearch />
             <p>Search and filter foods here</p>
         </div>
     );
---
+++ client/src/components/FoodSearch.jsx
@@ -0,0 +1,7 @@
+export default function FoodSearch() {
+    return (
+        <div>
+            <h2>Food Search Component Under Construction</h2>
+        </div>
+    )
+}
