f35fa35841da86abf6b01da11f9151dee8a41ce4
Kayla Hartland <kaylanhartland@gmail.com>
2025-12-15 18:23:51 -0500

    stylized addPost component

--- client/components/PostCard.jsx
+++ client/components/PostCard.jsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 export default function PostCards(props){
     const post = props.post
     return (
-        <div className="flex flow-root bg-gray-400 text-white mx-12 my-8 rounded-full">
+        <div className="flex flow-root bg-gray-500 text-white mx-12 my-8 rounded-full">
             <Link className="float-left hover:underline pl-8 text-2xl" href={`/community/${post._id}`}>{post.title}</Link>
             <div className="float-right grid grid-rows-3 grid-cols-1 pr-8 text-xs">
                 <div><p>Created: {post.created_at}</p></div>
                 <div><p>Updated: {post.updated_at}</p></div>
-                <div><p>User: </p></div>
+                <div><p>User: {post.user.first_name} {post.user.last_name} </p></div>
             </div>
         </div>
     );
