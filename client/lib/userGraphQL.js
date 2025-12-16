7d2ebef91f549c15b84dd4472f14dcc8984a6bff
christendeo <christen.deocampo@gmail.com>
2025-12-16 00:18:53 -0500

    Organized and stored user graphql mutations in one file for consistency

--- client/lib/userGraphQL.js
+++ client/lib/userGraphQL.js
@@ -1,6 +1,76 @@
 // For the front-end user mutations
 import {gql} from "@apollo/client";
 
+// New user creation
+export const ADD_USER = gql`
+    mutation AddUser(
+        $first_name: String!,
+        $last_name: String!,
+        $email: String!,
+        $password: String!,
+        $sex: String!,
+        $date_of_birth: String!,
+        $height: Float!,
+        $weight: Float!,
+        $activity_level: String!,
+        $diet_goal: String!,
+        $use_custom_target: Boolean,
+        $custom_target_calories: Float
+    ) {
+        addUser(
+            first_name: $first_name,
+            last_name: $last_name,
+            email: $email,
+            password: $password,
+            sex: $sex,
+            date_of_birth: $date_of_birth,
+            height: $height,
+            weight: $weight,
+            activity_level: $activity_level,
+            diet_goal: $diet_goal,
+            use_custom_target: $use_custom_target,
+            custom_target_calories: $custom_target_calories
+        ) {
+            _id
+            first_name
+            last_name
+            email
+            sex
+            date_of_birth
+            height
+            weight
+            activity_level
+            diet_goal
+            target_calories
+            use_custom_target
+            custom_target_calories
+            current_target_calories
+        }
+    }
+`;
+
+// User login
+export const LOGIN_USER = gql`
+    mutation LoginUser($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+            _id
+            first_name
+            last_name
+            email
+            sex
+            date_of_birth
+            height
+            weight
+            activity_level
+            diet_goal
+            target_calories
+            use_custom_target
+            custom_target_calories
+            current_target_calories
+        }
+    }
+`;
+
 // Edit profile
 export const EDIT_USER = gql`
     mutation EditUser(
@@ -15,6 +85,8 @@ export const EDIT_USER = gql`
         $activity_level: String
         $diet_goal: String
         $password: String
+        $use_custom_target: Boolean
+        $custom_target_calories: Float
     ) {
         editUser(
             _id: $_id
@@ -28,6 +100,8 @@ export const EDIT_USER = gql`
             activity_level: $activity_level
             diet_goal: $diet_goal
             password: $password
+            use_custom_target: $use_custom_target
+            custom_target_calories: $custom_target_calories
         ) {
             _id
             first_name
@@ -42,6 +116,9 @@ export const EDIT_USER = gql`
             target_calories
             createdAt
             updatedAt
+            use_custom_target
+            custom_target_calories
+            current_target_calories
         }
     }
 `;
