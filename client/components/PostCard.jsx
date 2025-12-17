0e76a3e072442681d86d467f1cecf7de7c0330bc
a-eryan <eryananthony@gmail.com>
2025-12-16 20:58:50 -0500

    tailwind presets for blogs applied

--- client/components/PostCard.jsx
+++ client/components/PostCard.jsx
@@ -1,15 +1,30 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import localFont from 'next/font/local';
+import tailwindCSS from '../lib/tailwindUIClasses';
 
-//pick a font?
+const NimbusFont = localFont({ 
+    src: '../public/NimbuDemo-Regular.otf',
+    variable: '--font-nimbus' 
+});
+
 export default function PostCards(props){
-    const post = props.post
+    const post = props.post;
+    
     return (
-        <div className="flex flow-root bg-gray-500 text-white mx-12 my-8 rounded-full">
-            <Link className="float-left hover:underline pl-8 text-2xl" href={`/community/${post._id}`}>{post.title}</Link>
-            <div className="float-right grid grid-rows-3 grid-cols-1 pr-8 text-xs">
-                <div><p>Created: {post.created_at}</p></div>
-                <div><p>Updated: {post.updated_at}</p></div>
-                <div><p>User: {post.user.first_name} {post.user.last_name} </p></div>
+        <div className={tailwindCSS.card}>
+            <div className="flex items-start justify-between gap-4">
+                <Link 
+                    className={`${tailwindCSS.link} text-xl font-semibold ${NimbusFont.className}`} 
+                    href={`/community/${post._id}`}
+                >
+                    {post.title}
+                </Link>
+                
+                <div className="text-s opacity-70 text-right flex-shrink-0">
+                    <div>Created: {post.created_at}</div>
+                    <div>Updated: {post.updated_at}</div>
+                    <div>By: {post.user.first_name} {post.user.last_name}</div>
+                </div>
             </div>
         </div>
     );
